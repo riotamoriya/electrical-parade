00461e5818333f80c0d8712cf3e91722ab3c1e38
riotamoriya <riotamoriya@gmail.com>
2024-02-28 14:43:26 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `electrical-parade`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
